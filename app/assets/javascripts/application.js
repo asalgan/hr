a59801233eafd687f14444eed3d82b52ffe36b20
Alan Salganik <asalganik1@gmail.com>
2014-04-24 19:38:43 -0500

    styled tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,6 +32,18 @@ $(document).ready(function() {
 
   $( "#tabs" ).tabs();
 
+  $('#tabs a').on('click', function(e)  {
+        var currentAttrValue = $(this).attr('href');
+ 
+        // Show/Hide Tabs
+        $('#tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+
 });
 
 
