63c5e429e734da5ba969b32278f551ad341c0b72
Alan Salganik <asalganik1@gmail.com>
2014-05-30 11:33:50 -0500

    Replaced .homepage div with @page_class.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
-
 	def index
 		if user_signed_in?
 			redirect_to current_company
 		else
+			@page_class = "home"
 			render layout: 'base'
 		end
 	end
