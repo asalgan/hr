c41099b7b960a43211ea2c9f8066a5581c71c035
Alan Salganik <asalganik1@gmail.com>
2014-05-30 10:20:37 -0500

    Fix Rails error by moving 'render' into else block.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
 	def index
 		if user_signed_in?
 			redirect_to current_company
+		else
+			render layout: 'base'
 		end
-		render layout: 'base'
 	end
 
 end
