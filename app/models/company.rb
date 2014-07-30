a739c3d2897e7742e5a69fe8282db0d394853cb3
Alan Salganik <asalganik1@gmail.com>
2014-07-30 16:09:51 -0500

    tabbing fixes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,14 +1,14 @@
 class Company < ActiveRecord::Base
-	belongs_to :user
-	has_many :jobs
-	has_many :applicants, through: :job_applications
-	has_many :job_applications
-	has_many :prospects
+  belongs_to :user
+  has_many :jobs
+  has_many :applicants, through: :job_applications
+  has_many :job_applications
+  has_many :prospects
 
-	has_attached_file :logo	
+  has_attached_file :logo 
   validates_attachment :logo,
-  	content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png']},
-  	:size => { :in => 0..500.kilobytes },
-  	:bucket => 'HRAPP'
+    content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png']},
+    :size => { :in => 0..500.kilobytes },
+    :bucket => 'HRAPP'
 
 end
