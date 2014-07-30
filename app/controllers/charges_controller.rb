f76a4804a6ae12f3deac36d3523876b686805ad8
Alan Salganik <asalganik1@gmail.com>
2014-07-30 16:05:18 -0500

    tabbing fixes

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,17 +1,17 @@
 class ChargesController < ApplicationController
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
-	def new
-	end
+  def new
+  end
 
-	def create
-	@user = current_user
+  def create
+  @user = current_user
 
-		if @user.create_customer(params[:stripeToken])
-			redirect_to root_url, notice: "Thank you for subscribing! You're ready to start hiring!"
-		else
-			redirect_to :back, notice: "An error occurred and we were unable to process your credit card. Please make sure you've entered all the correct information and try again."
-		end
-	end
+    if @user.create_customer(params[:stripeToken])
+      redirect_to root_url, notice: "Thank you for subscribing! You're ready to start hiring!"
+    else
+      redirect_to :back, notice: "An error occurred and we were unable to process your credit card. Please make sure you've entered all the correct information and try again."
+    end
+  end
 
 end
