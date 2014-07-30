1a757edabebef28eb0a223d7c0e3a9ae575d3e9e
Alan Salganik <asalganik1@gmail.com>
2014-07-30 16:10:16 -0500

    tabbing fixes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,22 +1,22 @@
 class Job < ActiveRecord::Base
-	belongs_to :company
-	has_many :applicants, through: :job_applications
-	has_many :job_applications
+  belongs_to :company
+  has_many :applicants, through: :job_applications
+  has_many :job_applications
 
-	def self.employment_types
-		["Full Time", "Part Time", "Temporary", "Contract", "Seasonal", "Internship", "Volunteer"]
-	end
+  def self.employment_types
+    ["Full Time", "Part Time", "Temporary", "Contract", "Seasonal", "Internship", "Volunteer"]
+  end
 
-	def self.employment_types_for_select
-		[["Full Time","Full Time"], ["Part Time","Part Time"], ["Temporary","Temporary"], ["Contract","Contract"], ["Seasonal","Seasonal"], ["Internship","Internship"], ["Volunteer","Volunteer"]]
-	end
+  def self.employment_types_for_select
+    [["Full Time","Full Time"], ["Part Time","Part Time"], ["Temporary","Temporary"], ["Contract","Contract"], ["Seasonal","Seasonal"], ["Internship","Internship"], ["Volunteer","Volunteer"]]
+  end
 
-	def self.experience_level
-		["Student (High School)", "Student (Undergrad)", "Student (Graduate)", "Entry Level", "Mid Level", "Experienced", "Manager/Supervisor", "Senior Manager/Supervisor", "Executive", "Senior"]
-	end
+  def self.experience_level
+    ["Student (High School)", "Student (Undergrad)", "Student (Graduate)", "Entry Level", "Mid Level", "Experienced", "Manager/Supervisor", "Senior Manager/Supervisor", "Executive", "Senior"]
+  end
 
-	def self.experience_level_for_select
-		[["Student (High School)","Student (High School)"], ["Student (Undergrad)","Student (Undergrad)"], ["Student (Graduate)","Student (Graduate)"], ["Entry Level","Entry Level"], ["Mid Level","Mid Level"], ["Experienced","Experienced"], ["Manager/Supervisor","Manager/Supervisor"], ["Senior Manager/Supervisor","Senior Manager/Supervisor"], ["Executive","Executive"], ["Senior","Senior"]]
-	end
+  def self.experience_level_for_select
+    [["Student (High School)","Student (High School)"], ["Student (Undergrad)","Student (Undergrad)"], ["Student (Graduate)","Student (Graduate)"], ["Entry Level","Entry Level"], ["Mid Level","Mid Level"], ["Experienced","Experienced"], ["Manager/Supervisor","Manager/Supervisor"], ["Senior Manager/Supervisor","Senior Manager/Supervisor"], ["Executive","Executive"], ["Senior","Senior"]]
+  end
 
 end
